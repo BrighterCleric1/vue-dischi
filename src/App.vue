4274b8700adfd4c9947a7b175e14f029a7bf37f2
BrighterCleric1 <simontemplar99@hotmail.it>
2021-07-15 17:20:37 +0200

    Facendo il  bonus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <Header />
-    <Main :albums="albums" />
+    <Header @selectInput="searchAlbumInput" @selectGenre="searchAlbumsGenre" />
+    <Main :albums="filteredGenres" />
   </div>
 </template>
 
@@ -9,6 +9,7 @@
 import axios from "axios";
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
+
 export default {
   name: "App",
   components: {
@@ -18,6 +19,8 @@ export default {
   data: function () {
     return {
       albums: [],
+      inputSelectGenre: "",
+      inputSelect: "",
     };
   },
   created() {
@@ -26,6 +29,30 @@ export default {
       .then((result) => {
         this.albums = result.data.response;
       });
+  },
+  computed: {
+    filteredGenres() {
+      if (
+        this.inputSelectGenre.length === 0 ||
+        this.inputSelectGenre === "null"
+      ) {
+        return this.albums;
+      }
+
+      return this.albums.filter((element) => {
+        return element.genre
+          .toLowerCase()
+          .includes(this.inputSelectGenre.toLowerCase());
+      });
+    },
+  },
+  methods: {
+    searchAlbumsGenre: function (selectStringGenre) {
+      this.inputSelectGenre = selectStringGenre.trim();
+    },
+    searchAlbumInput: function (inputString) {
+      this.inputSelect = inputString.trim();
+    },
   },
 };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,42 @@
 <template>
-  <header></header>
+  <header>
+    <div class="selectForGenres">
+      <label for="genres">Scegli un genere:</label>
+      <select
+        @change="$emit('selectGenre', selectStringGenre)"
+        v-model="selectStringGenre"
+        name="genres"
+        id="genres"
+      >
+        <option value="null">-</option>
+        <option value="pop">Pop</option>
+        <option value="rock">Rock</option>
+        <option value="jazz">Jazz</option>
+        <option value="metal">Metal</option>
+      </select>
+    </div>
+    <div>
+      <input
+        @keyup="$emit('selectInput', inputString)"
+        v-model="inputString"
+        placeholder="Cerca per titolo, autore o anno"
+        type="text"
+        name=""
+        id=""
+      />
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      selectStringGenre: "",
+      inputString: "",
+    };
+  },
 };
 </script>
 
